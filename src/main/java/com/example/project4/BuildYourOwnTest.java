618494189a73d7db27634a56b7ef1b5b707ecabd
OreoJuice <114830718+OreoJuice@users.noreply.github.com>
2022-11-21 22:40:42 -0500

    junit test fix

--- src/main/java/com/example/project4/BuildYourOwnTest.java
+++ src/main/java/com/example/project4/BuildYourOwnTest.java
@@ -1,9 +1,14 @@
 package com.example.project4;
 
 import org.junit.Test;
+//import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Class that tests the build your own pizza methods
+ * such as adding, removing, and calculating price
+ */
 public class BuildYourOwnTest {
 
     @Test
